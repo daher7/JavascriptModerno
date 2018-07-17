2548c34f056452ce96af021602ad6265fe340852
daher7 <daher7@gmail.com>
2018-07-17 21:34:25 +0200

    bluuweb

--- BluuwebFetchApi/js/app.js
+++ BluuwebFetchApi/js/app.js
@@ -1,6 +1,6 @@
 const btnObtener = document.getElementById('btnObtener');
 
-btnObtener.addEventListener('click', obtenerTxt);
+btnObtener.addEventListener('click', obtenerApi);
 
 function obtenerTxt() {
 
@@ -8,6 +8,20 @@ function obtenerTxt() {
         .then(res => res.text())
         .then(data => {
             const plantilla = `<p>${data}</p>`;
+            document.getElementById('contenido').innerHTML = plantilla;
+        })
+        .catch(error => console.log(error));
+}
+
+function obtenerApi() {
+    const url = 'https://randomuser.me/api/';
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.results[0]);
+            let plantilla = `<img src="${data.results[0].picture.large}" width="150px" class="img-fluid rounded-circle">
+                          <p><h4>${data.results[0].name.first} ${data.results[0].name.last}</h4></p>`;
             document.getElementById('contenido').innerHTML = plantilla;
         })
         .catch(error => console.log(error));
